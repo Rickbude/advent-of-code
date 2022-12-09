f6084d60baf5070db9ecd7fc1e8e1d4d7a58da74
Rick Budé <24381938+Rickbude@users.noreply.github.com>
2022-12-09 16:03:46 +0100

    Slight cleanup of puzzle9
    Use a small struct to represent a node, instead of a plain
    std::array<int,2>.

--- 2022/puzzle9/main.cpp
+++ 2022/puzzle9/main.cpp
@@ -8,7 +8,10 @@
 #include "aoc_utility.hpp"
 
 //Type of the node of the rope
-using node = std::array<int,2>;
+struct node{    
+    int x = 0;
+    int y = 0;    
+};
 
 //Returns the sign of the number:
 // +1 for positive numbers
@@ -27,8 +30,8 @@ int sign(int number){
 //Update the tail position. If the tail is touching the 
 //preceding 
 void update_tail(const node& head, node& tail){
-    int dx    = head[0]-tail[0];
-    int dy    = head[1]-tail[1];
+    int dx    = head.x-tail.x;
+    int dy    = head.y-tail.y;
 
     //Calculate squared distance
     int dist2 = dx*dx + dy*dy;
@@ -39,8 +42,8 @@ void update_tail(const node& head, node& tail){
     }
 
     //Update tail position
-    tail[1] += sign(dy);
-    tail[0] += sign(dx);    
+    tail.y += sign(dy);
+    tail.x += sign(dx);    
 }
 
 //Decode the elves's data stream
@@ -67,8 +70,6 @@ int main(int argc, char *argv[]){
     }else{
         nodes.resize(10);
     }
-    const node startpos{0,0};
-    std::fill(nodes.begin(),nodes.end(),startpos);
 
     while(std::getline(infs,line)){
         char direction = line[0];
@@ -80,16 +81,16 @@ int main(int argc, char *argv[]){
             node& head = *nodes.begin();
             switch(direction){
                 case 'U':
-                    head[0]++;
+                    head.x++;
                     break;
                 case 'D':
-                    head[0]--;
+                    head.x--;
                     break;
                 case 'L':
-                    head[1]--;
+                    head.y--;
                     break;
                 case 'R':
-                    head[1]++;
+                    head.y++;
                     break;
                 default:
                     throw std::runtime_error("Illegal move direction!!");
@@ -104,8 +105,8 @@ int main(int argc, char *argv[]){
             //Register the head and tail positions
             node& tail = nodes.back();
 
-            std::string curr_head_pos = std::to_string(head[0])  + "_" + std::to_string(head[0]);
-            std::string curr_tail_pos = std::to_string(tail[0])  + "_" + std::to_string(tail[1]);
+            std::string curr_head_pos = std::to_string(head.x)  + "_" + std::to_string(head.y);
+            std::string curr_tail_pos = std::to_string(tail.x)  + "_" + std::to_string(tail.y);
             head_visited.insert(curr_head_pos);
             tail_visited.insert(curr_tail_pos);
         }
