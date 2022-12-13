69752b4c31ba7c4b3ffa570315fda673830cf3e4
Rick Budé <24381938+Rickbude@users.noreply.github.com>
2022-12-13 02:38:09 +0100

    Fix A* search
    Code works for part1, but needs to be changed manually for part 2.
    Instructions are in the code. I have to think about a way of
    making this nicely.

--- 2022/puzzle12/main.cpp
+++ 2022/puzzle12/main.cpp
@@ -6,119 +6,108 @@
 #include <vector>
 #include <algorithm>
 #include <set>
-#include <queue>
+#include <list>
 #include "aoc_utility.hpp"
 #include <Eigen/Dense>
+#include <cmath>
 
 //Position type
 using pos_type = Eigen::Vector2i;
 
-
-//
+//Node structure
 struct Node{
+    //node id
     int         id;
-    pos_type    pos    ;
-    int         height = 0;
-    int         gscore = std::numeric_limits<int>::max();  //
-    int         fscore = std::numeric_limits<int>::max();  //
-    Node*       camefrom = nullptr;                        //The node we came from
-    //Make sorting Nodes possible
-    bool operator<(const Node& a) const
-    {
-        return (fscore < a.fscore);
-    }
+    //Node position                                                 
+    pos_type    pos;                                            
+    //Node position
+    int         height = 0;                                         
+    //Node height
+    double      gscore = std::numeric_limits<double>::infinity();   
+    //gScore is the cost of the cheapest path from start to n currently known.
+    double      fscore = std::numeric_limits<double>::infinity();   
+    //For node n, fScore[n] := gScore[n] + h(n). fScore[n] represents our current best guess as to
+    // how cheap a path could be from start to finish if it goes through n.
+    Node*       camefrom = nullptr;        
+    //The node we came from using the current best route
+    std::vector<Node*> neighbors;
 };
+
+//Small function used for comparing Node pointers
+bool compareNodes(const Node* a, const Node* b)
+{
+    return a->fscore < b->fscore;
+}
 
 //Map type
 using map_type = Eigen::Matrix<Node,-1,-1>;
 
-int distance(const pos_type& position, const pos_type& start){
-    return (position-start).cwiseAbs().sum();
+//Current heuristic: Manhattan distance.
+//Problem specific
+double heuristic(const Node* node, const pos_type& target){
+    return (node->pos - target).cwiseAbs().sum();
 }
 
-//Current heuristic: Manhattan distance
-int heuristic(const pos_type& position, const pos_type& target){
-    return (position-target).cwiseAbs().sum();
+//Cost of moving from one node to its neighbor
+//Problem specific
+//Reverse this condition for part 2
+double cost(const Node* current, const Node* neighbor){
+    if(neighbor->height - current->height <= 1){
+        return 1.0;
+    }else{
+        return 1000;
+    }
 }
 
-// A* finds a path from start to goal.
+// A* finds a path from start to goal. Relatively reusable implementation
 // Translated from pseudocode on https://en.wikipedia.org/wiki/A*_search_algorithm
-Node A_Star(pos_type start, pos_type goal, map_type& map){
-    
-    pos_type curr_pos   = start;
-    Node* startnode     = &map(curr_pos[0],curr_pos[1]);
+Node* A_Star(Node* startnode, const pos_type& goal){
     startnode->gscore   = 0;
-    startnode->fscore   = heuristic(curr_pos,goal);
-
+    startnode->fscore   = heuristic(startnode,goal);
 
     // The set of discovered nodes that may need to be (re-)expanded.
     // Initially, only the start node is known.
-    std::deque<Node> openSet;
-    openSet.push_back(*startnode);
+    std::list<Node*> openSet;
+    openSet.push_back(startnode);
+
+    int nsteps = 0;
+    int ninspect = 0;
     
     while(!openSet.empty()){
-        std::cout << "set size: " << openSet.size() << std::endl;
-        Node current = *openSet.begin();
-        curr_pos = current.pos;
-
-        std::cout << "set contains nodes with fscores:" <<std::endl;
-        for(const Node& node : openSet){
-            std::cout << node.fscore << ", ";
-        }
-        std::cout << std::endl;
+        // This operation can occur in O(Log(N)) time if openSet is a
+        // min-heap or a priority queue
+        auto it = std::min_element(openSet.begin(), openSet.end(),compareNodes);
+        nsteps ++;
+        
+        Node* current = *it;
+        openSet.erase(it);        
 
         //We reached the goal, we are done!
-        if(current.pos == goal){
-            std::cout << "WE ARE DONE" << std::endl;
-            return map(curr_pos[0],curr_pos[1]);
-        }
-
-        openSet.pop_front();  
+        if(current->pos == goal){
+        //for part 2: if(current->height == 0){
+            std::cout << "A* solution found within " << nsteps << " rounds, inspected " << ninspect << " elements " << std::endl;
+            return current;
+        }       
         
-        //Go in each direction
-        std::array<int,4> dx{-1, 1,  0, 0};
-        std::array<int,4> dy{ 0, 0, -1, 1};
+        //visit all the neighbors of this node
+        for(Node* neighbor : current->neighbors){
+            double tentative_gScore = current->gscore + cost(current,neighbor);
+            // This path to neighbor is better than any previous one. Record it!
+            if(tentative_gScore < neighbor->gscore){
+                neighbor->camefrom = current;
+                neighbor->gscore   = tentative_gScore;
+                neighbor->fscore   = tentative_gScore + heuristic(neighbor,goal);
 
-        for(int i = 0; i<4; i++){
-            
-            int row  = curr_pos[0] + dx[i];
-            int col  = curr_pos[1] + dy[i];            
-
-            //Only consider valid neighbors
-            if(row>=0 && col >= 0 && row <map.rows() && col < map.cols()){
-
-                pos_type neighbor_pos{row,col};
-                Node* neighbor    = &map(row,col);
-
-                //This is only a valid neigbour if we can actually climb it
-                if(std::abs(neighbor->height - current.height) > 1){
-                    continue;
+                //Check if this neighbor is not in the queue. If not, add it
+                auto result1 = std::find(openSet.begin(),openSet.end(),neighbor);                    
+                if(result1 == openSet.end()){
+                    openSet.push_back(neighbor);
+                    ninspect++;
                 }
-
-                int tentative_gScore = current.gscore + 1;
-
-                if(tentative_gScore < neighbor->gscore){
-                    neighbor->camefrom = &map(curr_pos[0],curr_pos[1]);
-                    neighbor->gscore   = tentative_gScore;
-                    neighbor->fscore   = tentative_gScore + heuristic(neighbor_pos,goal);
-
-                    //Check if this neighbor is not in the queue. If not, add it
-                    bool found = false;
-                    for(const Node& node : openSet){
-                        if(neighbor->id == node.id){
-                            found = true;                            
-                            break;
-                        }
-                    }
-                    if(!found){
-                        openSet.push_back(*neighbor);
-                        std::sort(openSet.begin(), openSet.end());
-                    }
-                }
-            }            
-        }
+            }                  
+        }        
     }
-    std::cout << "Failure" << std::endl;
+    throw std::runtime_error("A* failed to find a route");
 }
 
 
@@ -142,6 +131,7 @@ int main(int argc, char *argv[]){
         N_cols = line.size();
     }
 
+    //Start end and position
     pos_type startpos;
     pos_type endpos;
 
@@ -156,7 +146,8 @@ int main(int argc, char *argv[]){
     while(std::getline(infs,line)){
         int col = 0;
         for(char c : line){       
-            int height;     
+            int height;
+            //Convert height map to ints     
             if(c >= 'a' && c <= 'z'){
                 height = c - 'a';
             }else if(c == 'S'){
@@ -171,11 +162,19 @@ int main(int argc, char *argv[]){
                 throw std::runtime_error("invalid char encountered");
             }
             
+            //Create the node
             Node node;
             node.id     = counter;
             node.height = height;
             node.pos[0] = row;
             node.pos[1] = col;
+            
+            //Add the neighbors this node might have
+            if(col>0){           node.neighbors.push_back(&height_map(row,col-1));}
+            if(col<N_cols-1){    node.neighbors.push_back(&height_map(row,col+1));}
+            if(row>0){           node.neighbors.push_back(&height_map(row-1,col));}
+            if(row<N_rows-1){    node.neighbors.push_back(&height_map(row+1,col));}
+
             height_map(row,col) = node;
             counter++;
             col++;
@@ -183,26 +182,23 @@ int main(int argc, char *argv[]){
         row++;
     }
 
-    //Now, do A* search
-
+    //Now, do A* search    
+    Node* startnode = &height_map(startpos[0],startpos[1]);
+    Node* endnode = A_Star(startnode, endpos);
+   
+    //Reverse the search for part2
+    //Node* startnode = &height_map(endpos[0],endpos[1]);
+    //Node* endnode = A_Star(startnode, startpos);
     
-    
-    pos_type current_position = startpos;
-
-
-    Node endnode = A_Star(startpos, endpos, height_map);
-    Node* camefrom = endnode.camefrom;
-
+    //Count the number of steps by backtracing the route
     int n_steps = 0;
+    Node* camefrom = endnode->camefrom;
     while(camefrom != nullptr){
-        std::cout << "came through node " << camefrom->id << std::endl;
         camefrom  = camefrom->camefrom;
         n_steps++;
     }
 
     std::cout << "route took " << n_steps << " steps" << std::endl;
-
-   
 
     return 0;
 }
