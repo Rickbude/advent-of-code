b5fac6124281e057360663f4647d99484188c7d0
Rick Budé <24381938+Rickbude@users.noreply.github.com>
2022-12-15 10:31:34 +0100

    Fix puzzle 13 part 1, add part 2
    There was a small mistake in the parsing of nested empty sets,
    tripping up the comparisons. This didn't matter for the example,
    but it did for the actual input. The comparison code now takes
    const references instead of pointers, as does the print code.
    The unused parent pointers were removed

--- 2022/puzzle13/main.cpp
+++ 2022/puzzle13/main.cpp
@@ -12,7 +12,7 @@
 #include <memory>
 
 struct Node{
-    Node* parent = nullptr;
+    //Would be nice to try out std::vector<std::variant<Node,int>>
     std::vector<Node> nodes;
     int number = -1;
 };
@@ -55,10 +55,9 @@ void parse(Node* node, std::string in){
 
         //part 2: the closed set
         std::string part2 = in.substr(startpos+1,endpos-startpos-1);  
-        if(part2.size() > 0){
-            Node new_node;
-            new_node.parent = node;
-            node->nodes.push_back(new_node);
+        Node new_node;
+        node->nodes.push_back(new_node);
+        if(part2.size() > 0){            
             parse(&node->nodes.back(),part2);           
         }
 
@@ -73,7 +72,6 @@ void parse(Node* node, std::string in){
         while(std::getline(starting_items, item, ','))
         {
             Node new_node;
-            new_node.parent = node;
             new_node.number = std::stoi(item);
             node->nodes.push_back(new_node);
         }
@@ -81,17 +79,17 @@ void parse(Node* node, std::string in){
 }
 
 //Print the node, primarily to see if it was properly parsed
-std::string node_to_string(const Node* node){
+std::string node_to_string(const Node& node){
     std::string string;
-    if(node->nodes.size()>0){
+    if(node.nodes.size()>0){
         string += "[";
-        for(int i = 0; i< node->nodes.size(); i++){
-            string += node_to_string(&node->nodes[i]);
+        for(int i = 0; i< node.nodes.size(); i++){
+            string += node_to_string(node.nodes[i]);
         }
         string += "]";
     }else{
-        if(node->number >= 0){
-            string += " " + std::to_string(node->number) + " ";
+        if(node.number >= 0){
+            string += " " + std::to_string(node.number) + " ";
         }else{
             string += "[]";
         }        
@@ -99,63 +97,71 @@ std::string node_to_string(const Node* node){
     return string;
 }
 
-//Return -1 if wrong order, 0 if unknown (so proceed), 1 if right order, 2 if compare needs to start over
-int sorted(Node* node_a, Node* node_b){    
-    std::cout << "compare " << node_to_string(node_a) << " to " <<  node_to_string(node_b) << std::endl;
-    if(node_a->number != -1 && node_b->number != -1){
+//When comparing a list to an item, the number must be wrapped in a node,
+//and the comparison must be tried again
+Node wrap_number(int number){
+    Node parent  ;
+    Node daughter;
+    daughter.number = number;
+    parent.nodes.push_back(daughter);
+    return parent;
+}
+
+//Return -1 if wrong order, 0 if unknown (so proceed), 1 if right order
+int sorted(const Node& node_a, const Node& node_b){    
+    //std::cout << "compare " << node_to_string(node_a) << " to " <<  node_to_string(node_b) << std::endl;
+    if(node_a.number != -1 && node_b.number != -1){
         //Both nodes are a number --> see if the left number is smaller
-        if(node_a->number < node_b->number){
-            std::cout << "Left side is smaller, so inputs are in the right order" << std::endl;
+        if(node_a.number < node_b.number){
+            //std::cout << "Left side is smaller, so inputs are in the right order" << std::endl;
             return 1;
         }
-        if(node_b->number < node_a->number){
-            std::cout << "Right side is smaller, so inputs are not in the right order" << std::endl;
+        if(node_b.number < node_a.number){
+            //std::cout << "Right side is smaller, so inputs are not in the right order" << std::endl;
             return -1;
         }
         return 0;
-    }else if(node_a->number == -1 && node_b->number == -1){
+    }else if(node_a.number == -1 && node_b.number == -1){
         //Both values are a list
-        if(node_a->nodes.size() == 0 && node_b->nodes.size() == 0){
+        if(node_a.nodes.size() == 0 && node_b.nodes.size() == 0){
             return 0;
         }
 
-        for(int i = 0; i<node_a->nodes.size(); i++){
+        for(int i = 0; i<node_a.nodes.size(); i++){
             //right list has run out -> not the right order
-            if(i+1 > node_b->nodes.size()){
-                std::cout << "Right side ran out of items, so inputs are not in the right order" << std::endl;
+            if(i+1 > node_b.nodes.size()){
+                //std::cout << "Right side ran out of items, so inputs are not in the right order" << std::endl;
                 return -1;
             }
-            int is_sorted = sorted(&node_a->nodes[i],&node_b->nodes[i]);
+            int is_sorted = sorted(node_a.nodes[i],node_b.nodes[i]);
             if(is_sorted != 0){
                 return is_sorted;
             }
         }
         //Same sized lists -> continue
-        if(node_a->nodes.size() == node_b->nodes.size()){
+        if(node_a.nodes.size() == node_b.nodes.size()){
             return 0;
         }else{
-            std::cout << "Left side ran out of items, so inputs are in the right order" << std::endl;
+            //std::cout << "Left side ran out of items, so inputs are in the right order" << std::endl;
             return 1;
         }
-    }else if(node_a->number == -1){
-        Node new_node;
-        new_node.parent = node_b;
-        new_node.number = node_b->number;
-        node_b->number = -1;
-        node_b->nodes.push_back(new_node);
-        std::cout << "Changing int to list and retry" << std::endl;
-        return 2;
+    }else if(node_a.number == -1){
+        //Comparing integer to list -> wrap number in a node and retry
+        //std::cout << "Changing int to list and retry" << std::endl;
+        Node new_node = wrap_number(node_b.number);
+        return sorted(node_a,new_node);
     }else{
-        Node new_node;
-        new_node.parent = node_a;
-        new_node.number = node_a->number;
-        node_a->nodes.push_back(new_node);
-        node_a->number = -1;
-        std::cout << "Changing int to list and retry" << std::endl;
-        return 2;
+        //Comparing integer to list -> wrap number in a node and retry
+        //std::cout << "Changing int to list and retry" << std::endl;
+        Node new_node = wrap_number(node_a.number);
+        return sorted(new_node,node_b);
     }
     std::cout << "wrong comparison????" << std::endl;
+}
 
+//Small function that makes sorting possible
+bool is_sorted(const Node& node_a, const Node& node_b){
+    return sorted(node_a,node_b) == 1;
 }
 
 //Distress Signal
@@ -172,52 +178,48 @@ int main(int argc, char *argv[]){
     std::fstream infs("input.txt");    
     
     std::string line;
-    int pairnum = 0;
+    std::vector<Node> nodes;
 
-    std::array<Node,2> nodes;
-
-    int setnum = 1;
-    int unsorted_indices_sum = 0;
     //Read in the data
-    while(std::getline(infs,line))    {
+    while(std::getline(infs,line)){
         //split input at new line
         if(line.size() == 0){
-            std::cout << "Checking set " << setnum << std::endl;
-            while(sorted(&nodes[0],&nodes[1]) == 2){
-                continue;
-            }
-            if(sorted(&nodes[0],&nodes[1]) == 1){                
-                unsorted_indices_sum+=setnum;
-                std::cout << "list is sorted!!" << std::endl;
-            }else{
-                std::cout << "list is not sorted" << std::endl;
-            }
-            pairnum = 0;
-            setnum++;
             continue;
         }
 
-        nodes[pairnum].nodes.clear();
-        nodes[pairnum].number = -1;
-
-        parse(&nodes[pairnum],line);
-        //print(&nodes[pairnum]);
-
-        pairnum++;
-        
+        Node new_node;
+        parse(&new_node,line.substr(1,line.size()-2));        
+        nodes.push_back(new_node);       
     }
-    std::cout << "Checking set " << setnum << std::endl;
-    while(sorted(&nodes[0],&nodes[1]) == 2){
-        continue;
-    }
-    if(sorted(&nodes[0],&nodes[1]) == 1){
-        unsorted_indices_sum+=setnum;
-        std::cout << "list is sorted!!" << std::endl;
-    }else{
-        std::cout << "list is not sorted" << std::endl;
+
+    int unsorted_indices_sum = 0;
+    for(int i = 0; i<(nodes.size()/2); i++){
+        if(is_sorted(nodes[i*2],nodes[i*2+1])){
+            unsorted_indices_sum += (i+1);
+        }
     }
 
     std::cout << "part 1 solution: " << unsorted_indices_sum << std::endl;
+
+    //For part 2, insert 2 extra nodes: the "markers"
+    Node startnode = wrap_number(2);
+    Node endnode   = wrap_number(6);
+    nodes.push_back(startnode);
+    nodes.push_back(endnode);
+    std::sort(nodes.begin(), nodes.end(), is_sorted);
+
+    int startnode_index = -1;
+    int endnode_index   = -1;
+    for(int i = 0; i<nodes.size(); i++){
+        if(sorted(nodes[i],startnode) == 0){
+            //start marker located
+            startnode_index = i+1;
+        }else if(sorted(nodes[i],endnode) == 0){
+            //end marker located
+            endnode_index   = i+1;
+        }
+    }
+    std::cout << "part 2 solution: " << startnode_index*endnode_index << std::endl;
 
     return 0;
 }
