1f85aad3d39b91dcad4faa8580f567800fd6523c
Rick Budé <24381938+Rickbude@users.noreply.github.com>
2022-12-27 12:45:13 +0100

    Continue puzzle20
    Split off circular iterator advance into its own function

--- 2022/puzzle20/main.cpp
+++ 2022/puzzle20/main.cpp
@@ -2,10 +2,27 @@
 #include <string>
 #include <fstream>
 #include <list>
-#include <regex>
 #include <cassert>
-#include <Eigen/Dense>
+#include <algorithm>
 #include "aoc_utility.hpp"
+
+void advance_circular(std::list<int>::iterator &it, std::list<int>& list, int shift){
+    if(shift>0){
+        for(int i = 0; i<=shift; i++){
+            std::advance(it,1);
+            if(it == list.end()){
+                it = list.begin();
+            }
+        }
+    }else{
+        for(int i = 0; i>shift; i--){
+            std::advance(it,-1);
+            if(it == list.begin()){
+                it = list.end();
+            }
+        }
+    }
+}
 
 //Grove Positioning System
 //This script needs as input whether it needs to run part1, or part 2.
@@ -22,32 +39,36 @@ int main(int argc, char *argv[]){
     
     std::string line;
 
-    //Input follows pattern: xx,yy,zz
-    std::regex line_expr("^Blueprint (\\d+): Each ore robot costs (\\d+) ore. Each clay robot costs (\\d+) ore. Each obsidian robot costs (\\d+) ore and (\\d+) clay. Each geode robot costs (\\d+) ore and (\\d+) obsidian.$");
-
     std::list<int> numbers;
     //Read in the data
     while(std::getline(infs,line)){
         numbers.push_back(std::stoi(line));
     }
 
-    //Now do the circular shifts
-    for(int i = 0; i<numbers.size(); i++){
-        int number = *numbers.begin();
-        std::iterator it;
-        if(number > 0){
-            it = numbers.begin();
-        }else{
-            it = numbers.end();
-        }
-        std::advance(it,number);
-        numbers.insert(it,number);
-    }
-
-    for(const auto& it : numbers){
-        std::cout << it << std::endl;
-    }
-
+    std::list<int> initial_numbers = numbers;
     
+    //Now do the circular shifts
+    for(int number : initial_numbers){
+        std::list<int>::iterator it = std::find(numbers.begin(), numbers.end(),number);
+        std::list<int>::iterator it2 = it;
+        advance_circular(it2,numbers,number%numbers.size());
+        numbers.splice(it2,numbers,it);
+    }
+
+    std::list<int>::iterator it = std::find(numbers.begin(), numbers.end(), 0);
+    std::list<int>::iterator it1000 = it;
+    std::list<int>::iterator it2000 = it;
+    std::list<int>::iterator it3000 = it;
+    advance_circular(it1000,numbers,1000-1);
+    advance_circular(it2000,numbers,2000-1);
+    advance_circular(it3000,numbers,3000-1);
+
+    std::cout << "1000th number after 0: " << *it1000 << std::endl;
+    std::cout << "2000th number after 0: " << *it2000 << std::endl;
+    std::cout << "3000th number after 0: " << *it3000 << std::endl;
+    std::cout << "Sum: " << (*it1000) + (*it2000) + (*it3000) <<std::endl;
+
+    //Too high: 11645
+
     return 0;
 }
