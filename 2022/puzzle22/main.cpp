e1f272a90d3e29c4feb138fb44bf0979e3c5ff4b
Rick Budé <24381938+Rickbude@users.noreply.github.com>
2023-01-03 18:29:09 +0100

    Remove Eigen dependency of puzzle22
    Replace with a hashmap, with as key (row*10000+column), and as
    value a node. Each node knows its own row and column, whether it
    is a wall or not, and knows its neighbours in each wind direction.
    This hopefully prepares the code better for part 2.

--- 2022/puzzle22/main.cpp
+++ 2022/puzzle22/main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <list>
-#include <cassert>
 #include <vector>
 #include <algorithm>
 #include <unordered_map>
 #include "aoc_utility.hpp"
-#include <Eigen/Dense>
 
 enum Facing{
     right=0,
@@ -16,53 +13,52 @@ enum Facing{
     up=3
 };
 
-bool try_move(const Eigen::Matrix<char,-1,-1>& map, int& row, int& col, Facing facing){
-    int new_row = row;
-    int new_col = col;
-    switch(facing){
-        case up:
-            new_row--;
-            if(new_row==-1){
-                new_row = map.rows()-1;
-            }
-            break;
-        case down:
-            new_row++;
-            if(new_row==map.rows()){
-                new_row = 0;
-            }
-            break;
-        case left:            
-            new_col--;
-            if(new_col==-1){
-                new_col = map.cols()-1;
-            }
-            break;
-        case right:
-            new_col++;
-            if(new_col==map.cols()){
-                new_col = 0;
-            }
-            break;
-    }
+struct Node{
+    int row;
+    int col;
+    std::array<int,4> neighbours;
+    char tile;
+};
 
-    if(map(new_row,new_col) == '#'){
-        //We hit a wall if we would take this step -> stop
-        return false;
-    }else if(map(new_row,new_col) == ' '){
-        //We hit open space -> loop around to the other edge of the map
-        if(try_move(map,new_row,new_col,facing)){
-            row = new_row;
-            col = new_col;
-            return true;
-        }else{
-            return false;
-        }
-    }else{
-        //attempted step was successful, take it
-        row = new_row;
-        col = new_col;
+//"hashing function" for (row,col) to id. This assumes there are less than 10.000
+//columns in the map
+int row_col_to_id(int row, int col){
+    return 10000*row + col;
+}
+int id_to_row(int id){
+    return id/10000;
+}
+int id_to_col(int id){
+    return id%10000;
+}
+
+//Try to take a step in a given direction (facing). Return false if the step
+//would hit a wall ('#'), true otherwise.
+bool try_move(std::unordered_map<int,Node>& map, int& row, int& col, Facing facing){
+    int id = row_col_to_id(row,col);
+    const Node& neighbour = map.at(map.at(id).neighbours[facing]);
+    if(neighbour.tile != '#'){
+        row = neighbour.row;
+        col = neighbour.col;
         return true;
+    }else{
+        return false;
+    }
+}
+
+//Print the map (for debugging purposes)
+void print_map(std::unordered_map<int,Node>& map, int Nrows, int Ncols){
+    for(int row = 0; row<Nrows; row++){
+        std::string line;
+        for(int col = 0; col<Ncols; col++){
+            int id = row_col_to_id(row,col);
+            if(map.find(id) != map.end()){
+                line += map.at(id).tile;
+            }else{
+                line += ' ';
+            }
+        }
+        std::cout << line << std::endl;
     }
 }
 
@@ -84,11 +80,12 @@ int main(int argc, char *argv[]){
     std::vector<std::pair<int,int>> instructions;
 
     //Load the map into a matrix, and simultaneously determine the maximum matrix dimensions
-    Eigen::Matrix<char,-1,-1> map (1000,1000);
-    map.setConstant(' ');
-    int max_x = 0;
-    int max_y = 0;
-    int x = 0;
+    std::unordered_map<int,Node> map;
+    Node node;
+    int Nrows = 0;
+    int Ncols = 0;
+    int row = 0;
+    
     while(std::getline(infs,line)){
         if(line == ""){
             //Part 2 starts: get the instructions
@@ -119,29 +116,56 @@ int main(int argc, char *argv[]){
             break;
         }else{
             //Part 1 contains the map
-            for(int y = 0; y<line.size(); y++){
-                map(x,y) = line[y];
-                max_y = std::max(y,max_y);
+            for(int col = 0; col<line.size(); col++){
+                Ncols = std::max(col+1,Ncols);
+                if(line[col] != ' '){
+                    node.row  = row;
+                    node.col  = col;
+                    node.tile = line[col];
+                    int id    = row_col_to_id(row,col);   
+                    map[id]  = node;
+                }
             }
-            max_x = std::max(x,max_x);
-            x++;            
+            Nrows = std::max(row+1,Nrows);
+            row++;            
         }
     }
-    //Resize the matrix to only fit the current map
-    map.conservativeResize(max_x+1,max_y+1);
+
+    //Determine for each node its neighbours. This process is the main difference between part 1 and 2
+    for(auto& node : map){
+        for(int facing = right; facing<=up; facing = Facing(facing+1)){
+            int new_row = node.second.row;
+            int new_col = node.second.col;
+            if(part == 1){
+                int d_row = (facing%2 == 1) ? 2 - facing : 0;
+                int d_col = (facing%2 == 0) ? 1 - facing : 0;
+                do{
+                    new_row = aoc::mod(new_row+d_row,Nrows);
+                    new_col = aoc::mod(new_col+d_col,Ncols);
+                }while(map.find(row_col_to_id(new_row,new_col)) == map.end());                                                    
+            }else{
+
+            } 
+            node.second.neighbours[facing] = row_col_to_id(new_row,new_col);
+        }          
+    }
     
     //Move to the starting coordinates
     int start_row = 0;
-    int start_col = 0;
+    int start_col;
     Facing facing = right;
-    try_move(map,start_row,start_col,facing);
-    std::cout << "starting at (" <<start_row << "," << start_col << ")"  << std::endl;
+    for(start_col = 0; start_col<10000; start_col++){
+        int id = row_col_to_id(start_row,start_col);
+        if(map.find(id) != map.end() && map.at(id).tile == '.'){
+            std::cout << "starting at (" <<start_row << "," << start_col << ")"  << std::endl;
+            break;
+        }
+    }    
 
-    //Mark our steps in the map  
-    std::string signs = ">v<^";
+    
 
     //Step through the instructions one at a time
-    int row = start_row;
+    row = start_row;
     int col = start_col;
     for(const auto& instruction : instructions){
 
@@ -151,7 +175,9 @@ int main(int argc, char *argv[]){
         
         //First make N steps
         for(int step = 0; step<steps; step++){
-            map(row,col) = signs[facing];
+            //Mark our steps in the map  
+            const std::string signs = ">v<^";
+            map.at(row_col_to_id(row,col)).tile = signs[facing];
             bool did_move = try_move(map,new_row,new_col,facing);
             if(did_move){                
                 row = new_row;
@@ -166,7 +192,7 @@ int main(int argc, char *argv[]){
     }
 
     //Print the map
-    std::cout << map << std::endl;
+    print_map(map,Nrows,Ncols);
 
     std::cout << "Password: " << std::to_string((row+1)*1000 + (col+1)*4 + facing) << std::endl;
 
