478515e1ba0569bac7aee0eb1b3639e67d2333d3
Roel Budé <roel.bude@antennex.tech>
2023-12-01 13:47:25 +0100

    Even shorter

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -5,10 +5,8 @@ use std::collections::HashMap;
 //Get the key from a single line, which is a concatenation of the first and last
 //digit. E.g. a1b2c3d4e5f -> 15
 fn get_subkey(part: &str) -> i32{
-	let firstvalopt: char = part.chars().find(|&c| c.is_numeric()).unwrap();
-	let firstval: i32 = firstvalopt as i32 - '0' as i32;
-	let lastvalopt: char = part.chars().rev().find(|&c| c.is_numeric()).unwrap();
-	let lastval: i32 = lastvalopt as i32 - '0' as i32;
+	let firstval: i32 = part.chars().find(|&c| c.is_numeric()).unwrap() as i32 - '0' as i32;
+	let lastval: i32 = part.chars().rev().find(|&c| c.is_numeric()).unwrap() as i32 - '0' as i32;
 	return firstval*10 + lastval;
 }
 
