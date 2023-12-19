26cf7b8a716714ec90b1d9e6d80da8e5cc28cdae
Rick Budé <24381938+Rickbude@users.noreply.github.com>
2023-12-19 09:33:45 +0100

    Solution day18
    Use the shoelace algorithm to calculate the area

---
+++ 2023/day18/src/main.rs
@@ -0,0 +1,119 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash, PartialOrd, Ord, Debug)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+//Map input character to direction
+fn map_direction(s: &str) -> Option<Direction> {
+    match s {
+        "U" | "3" => Some(Direction::Up),
+        "D" | "1" => Some(Direction::Down),
+        "L" | "2" => Some(Direction::Left),
+        "R" | "0" => Some(Direction::Right),
+        _ => None,
+    }
+}
+
+//Get row/col offset for each direction
+fn get_delta(direction: Direction) -> (i64, i64) {
+    match direction {
+        Direction::Up => (-1, 0),
+        Direction::Down => (1, 0),
+        Direction::Right => (0, 1),
+        Direction::Left => (0, -1),
+    }
+}
+
+//Position on the map: (x,y)
+type Pos = (i64, i64);
+//Instruction (# of steps in which direction)
+type Instruction = (Direction, i64);
+
+//Calculate the answer to part 1 / 2
+fn calculate_key(part: usize, filename: &str) -> Option<usize> {
+    //Open input file
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    //Parse input file into vector of instructions
+    let instructions: Vec<Instruction> = contents
+        .lines()
+        .map(|line| {
+            let parts: Vec<&str> = line.trim().split_whitespace().collect();
+            if part == 1 {
+                //In part 1, read direction and steps "normally"
+                let direction = map_direction(parts[0]).unwrap();
+                let steps = parts[1].parse::<i64>().unwrap();
+                (direction, steps)
+            } else {
+                //In part two: the sixth digit of the color code encodes
+                //direction, and the first five hex digits the number of steps.
+                let color_code = parts[2].to_string();
+                let steps = i64::from_str_radix(&color_code[2..7], 16).unwrap();
+                let direction = map_direction(&color_code[7..8]).unwrap();
+                (direction, steps)
+            }
+        })
+        .collect();
+
+    //Get the corner points of the map, and the total length of the path
+    let mut pos: Pos = (0, 0);
+    let mut corners: Vec<Pos> = Vec::new();
+    let mut path_len = 0;
+    instructions.iter().for_each(|(direction, steps)| {
+        let delta = get_delta(*direction);
+        corners.push(pos);
+        pos.0 += delta.0 * steps;
+        pos.1 += delta.1 * steps;
+        path_len += steps;
+    });
+
+    //Use the "Shoelace method" to calculate the polygon area defined by the set
+    //of corner points: https://en.wikipedia.org/wiki/Shoelace_formula:
+    //              | x1 x2 |   | x2 x2 |
+    //    A = 1/2 * | y1 y2 | + | y2 y3 |
+    //Note that this assumes a line of 0 width
+    let inner_area = corners
+        .windows(2)
+        .map(|p| p[0].0 * p[1].1 - p[0].1 * p[1].0)
+        .sum::<i64>()
+        .abs()
+        / 2;
+
+    //Account for the finite line width: each straight step along the trail adds
+    //0.5 squares extra, so add line_length/2. Furthermore, each corner adds an
+    //additional +/- 0.25 squares, depending on whether it curves inwards or
+    //outwards. Since we form a closed loop, there are exactly 4 more outer
+    //corners than inner corners, so add 1 (4*0.25)
+    let area = (inner_area + path_len / 2 + 1) as usize;
+    Some(area)
+}
+
+fn main() -> std::io::Result<()> {
+    //Calculate the answers to part 1 and 2
+    println!("Key part 1: {}", calculate_key(1, "src/input.txt").unwrap());
+    println!("Key part 2: {}", calculate_key(2, "src/input.txt").unwrap());
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pt1() {
+        assert_eq!(calculate_key(1, "src/test.txt").unwrap(), 62);
+    }
+
+    #[test]
+    fn test_pt2() {
+        assert_eq!(calculate_key(2, "src/test.txt").unwrap(), 952408144115);
+    }
+}
