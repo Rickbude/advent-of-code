dd34f4ef2c3d47d647c089207b8f5d553ff87374
Rick Budé <24381938+Rickbude@users.noreply.github.com>
2023-12-26 22:41:22 +0100

    Change day21 solution to polynomial fit
    Use a polynomial fit to extrapolate the number of visitable
    plots for large numbers of steps, as suggested on the solution
    thread. Simplifies code quite a bit.
    Polynomial fit only starts to work for the test input for
    steps = n*rows+6, with n>=4. For the real input it starts right
    away at n = 0. The tests pass much faster now.

--- 2023/day21/src/main.rs
+++ 2023/day21/src/main.rs
@@ -16,11 +16,8 @@ enum Direction {
 //Position on the map: (x,y)
 type Pos = (i64, i64);
 
-//submap row/column + position on that submap
-type State = (Pos, Pos);
-
 //Try to take a step forward.
-fn step_forward(old_pos: State, direction: Direction, rows: usize, cols: usize) -> State {
+fn step_forward(old_pos: Pos, direction: Direction) -> Pos {
     //Define what a step in each direction does to row/col number
     let (d_row, d_col): (i64, i64) = match direction {
         Direction::North => (-1, 0),
@@ -28,44 +25,27 @@ fn step_forward(old_pos: State, direction: Direction, rows: usize, cols: usize) 
         Direction::South => (1, 0),
         Direction::West => (0, -1),
     };
-    //Potentially step to a new map copy / garden tile
-    let ((map_row, map_col), (row, col)) = old_pos;
-    let mut new_row = row + d_row;
-    let mut new_col = col + d_col;
-    let mut new_map_row = map_row;
-    let mut new_map_col = map_col;
-    if new_row >= rows as i64 || new_col >= cols as i64 || new_row < 0 || new_col < 0 {
-        new_map_row += d_row;
-        new_map_col += d_col;
-        new_row = new_row.rem_euclid(rows as i64);
-        new_col = new_col.rem_euclid(cols as i64);
-    }
-    ((new_map_row, new_map_col), (new_row, new_col))
+    let (row, col) = old_pos;
+    (row + d_row, col + d_col)
 }
 
-fn count_visitable(
-    map: &Vec<char>,
-    rows: usize,
-    cols: usize,
-    max_steps: usize,
-) -> Option<HashMap<Pos, usize>> {
+fn count_visitable(map: &Vec<char>, rows: usize, cols: usize, max_steps: usize) -> Option<usize> {
     let start_index = map.iter().position(|c| c == &'S')?;
     let start_row = start_index / cols;
     let start_col = start_index % cols;
 
     //Keep track of states we have seen before
-    let mut visited_squares: HashSet<State> = HashSet::new();
+    let mut visited_squares: HashSet<Pos> = HashSet::new();
 
     //List with states under consideration, accompanied by the current shortest
     //distance. Stored on a min-heap
-    let mut active_states: BinaryHeap<Reverse<(usize, State)>> = BinaryHeap::new();
-    let mut min_distances: HashMap<State, usize> = HashMap::new();
+    let mut active_states: BinaryHeap<Reverse<(usize, Pos)>> = BinaryHeap::new();
+    let mut min_distances: HashMap<Pos, usize> = HashMap::new();
 
     //Insert start position and define end position
     let start_pos: Pos = (start_row as i64, start_col as i64);
-    let start_state = ((0, 0), start_pos);
-    active_states.push(Reverse((0, start_state)));
-    min_distances.insert(start_state, 0);
+    active_states.push(Reverse((0, start_pos)));
+    min_distances.insert(start_pos, 0);
 
     loop {
         //All nodes were visited, but the destination was never reached. Stop!
@@ -82,19 +62,19 @@ fn count_visitable(
         }
 
         //List of new states to add to the active states
-        let mut new_states: Vec<State> = Vec::new();
+        let mut new_states: Vec<Pos> = Vec::new();
 
-        new_states.push(step_forward(position, Direction::North, rows, cols));
-        new_states.push(step_forward(position, Direction::East, rows, cols));
-        new_states.push(step_forward(position, Direction::South, rows, cols));
-        new_states.push(step_forward(position, Direction::West, rows, cols));
+        new_states.push(step_forward(position, Direction::North));
+        new_states.push(step_forward(position, Direction::East));
+        new_states.push(step_forward(position, Direction::South));
+        new_states.push(step_forward(position, Direction::West));
 
         //Add the new, unseen states to the list with active states
         for state in new_states {
-            let (row, col) = state.1;
             let new_distance = distance + 1;
-            let new_index = (row * cols as i64 + col) as usize;
-            if map[new_index] == '#' {
+            let row = state.0.rem_euclid(rows as i64) as usize;
+            let col = state.1.rem_euclid(cols as i64) as usize;
+            if map[row * cols + col] == '#' {
                 continue;
             }
             let old_distance = min_distances.entry(state).or_insert(new_distance);
@@ -108,22 +88,21 @@ fn count_visitable(
         }
     }
 
-    //Count the visitable squares, group per garden tile
-    let mut visitable_per_submap: HashMap<Pos, usize> = HashMap::new();
-    for ((submap, _), distance) in min_distances.iter() {
-        if distance % 2 == max_steps % 2 {
-            visitable_per_submap
-                .entry(*submap)
-                .and_modify(|v| *v += 1)
-                .or_insert(1);
-        }
-    }
-
-    Some(visitable_per_submap)
+    Some(
+        min_distances
+            .values()
+            .filter(|v| *v % 2 == max_steps % 2)
+            .count(),
+    )
 }
 
 //Calculate the answer to part 1 / 2
-fn calculate_key(part: usize, max_steps: usize, filename: &str) -> Option<usize> {
+fn calculate_key(
+    part: usize,
+    max_steps: usize,
+    n_values: Option<Vec<usize>>,
+    filename: &str,
+) -> Option<usize> {
     //Open input file
     let mut file = File::open(filename).unwrap();
     let mut contents = String::new();
@@ -134,44 +113,50 @@ fn calculate_key(part: usize, max_steps: usize, filename: &str) -> Option<usize>
     let cols = contents.lines().next()?.len();
     let map: Vec<char> = contents.replace("\n", "").chars().collect();
 
-    if part == 1 {
-        Some(count_visitable(&map, rows, cols, max_steps)?.values().sum())
+    if part == 1 || n_values.is_none() {
+        Some(count_visitable(&map, rows, cols, max_steps)?)
     } else {
-        //Analysis of the test data and visited squares per garden revealed that
-        //the visited square grows like a diamond (the actual input even more so
-        //than the test input, where the corners might deviate slightly). There
-        //are a few distinct gardens:
-        //    /T\       T,L,R,B: top, left, right bottom.    4 x
-        //   /<O>\      /\[] : "outer edges"                 N x
-        //   LOEOR      <>{} : "inner edges"                 (N-1) x
-        //   [{O}]      E/O  : Even and Odd inner blocks     (N-1)^2 x and N^2 x
-        //    [B]
+        //For part 2, assume that the pattern eventually settles into a
+        //quadratic equation:   y = ax^2 + bx + c
+        //For the test input this pattern is achieved for n>=4, while
+        //for the actual input, this pattern is already found from n=0
 
-        //Get the tiles for the 2*rows + steps % rows, which is the first time we
-        //see all of the above tiles
-        let n = max_steps / rows;
-        let rem = max_steps % rows;
-        let mut map = count_visitable(&map, rows, cols, 2 * rows + rem)?;
-        //Fill in the empty tiles
-        for map_row in -2..3 {
-            for map_col in -2..3 {
-                map.entry((map_row, map_col)).or_insert(0);
-            }
-        }
+        //Determine a,b and c, by sampling the problem with 3 different
+        //values for x (n1 -> y1, n2 -> y2, n3 -> y3), and use linear
+        //algebra to find a,b,c by solving the Vandermonde matrix:
+        // [ n1^2  n1  1 ] [a]   [ y1 ]
+        // [ n2^2  n2  1 ] [b] = [ y2 ]
+        // [ n3^3  n3  1 ] [c]   [ y3 ]
 
-        //E and O tiles
-        let core = (n - 1) * (n - 1) * map.get(&(0, 0))? + n * n * map.get(&(1, 0))?;
-        // T,L,B,R tiles
-        let corners =
-            map.get(&(2, 0))? + map.get(&(-2, 0))? + map.get(&(0, 2))? + map.get(&(0, -2))?;
-        // <,>,{,} tiles
-        let edges_o = (n - 1)
-            * (map.get(&(1, 1))? + map.get(&(-1, 1))? + map.get(&(1, -1))? + map.get(&(-1, -1))?);
-        // /,\,[,] tiles
-        let edges_i = n
-            * (map.get(&(1, 2))? + map.get(&(1, -2))? + map.get(&(-1, 2))? + map.get(&(-1, -2))?);
+        // xn = n*rows + steps%rows
+        let n_values_copy = n_values?.clone();
+        let y_values: Vec<usize> = n_values_copy
+            .clone()
+            .iter()
+            .map(|n| n * rows + max_steps % rows)
+            .map(|x| count_visitable(&map, rows, cols, x).unwrap())
+            .collect();
 
-        Some(core + corners + edges_i + edges_o)
+        //Unpack the n and y vectors for shorter mathematical expressions below
+        let n1 = n_values_copy[0] as i64;
+        let n2 = n_values_copy[1] as i64;
+        let n3 = n_values_copy[2] as i64;
+        let y1 = y_values[0] as i64;
+        let y2 = y_values[1] as i64;
+        let y3 = y_values[2] as i64;
+
+        //These expressions have been derived using Matlab's symbolic math
+        //package. A shorter equivalent might be found with other extrapolation methods
+        let determinant = (n1 - n2) * (n1 - n3) * (n2 - n3);
+        let a = n1 * (y3 - y2) + n2 * (y1 - y3) + n3 * (y2 - y1);
+        let b = n1 * n1 * (y2 - y3) + n2 * n2 * (y3 - y1) + n3 * n3 * (y1 - y2);
+        let c = n1 * n1 * (y3 * n2 - y2 * n3)
+            + n2 * n2 * (y1 * n3 - y3 * n1)
+            + n3 * n3 * (y2 * n1 - y1 * n2);
+
+        let n = (max_steps / rows) as i64;
+        let y = (a * n * n + b * n + c) / determinant;
+        Some(y as usize)
     }
 }
 
@@ -179,11 +164,11 @@ fn main() -> std::io::Result<()> {
     //Calculate the answers to part 1 and 2
     println!(
         "Key part 1: {}",
-        calculate_key(1, 64, "src/input.txt").unwrap()
+        calculate_key(1, 64, None, "src/input.txt").unwrap()
     );
     println!(
         "Key part 2: {}",
-        calculate_key(2, 26501365, "src/input.txt").unwrap()
+        calculate_key(2, 26501365, Some(vec![0, 1, 2]), "src/input.txt").unwrap()
     );
     Ok(())
 }
@@ -194,36 +179,51 @@ mod tests {
 
     #[test]
     fn test_pt1() {
-        assert_eq!(calculate_key(1, 6, "src/test.txt").unwrap(), 16);
+        assert_eq!(calculate_key(1, 6, None, "src/test.txt").unwrap(), 16);
     }
 
     #[test]
     fn test_pt2_1() {
-        assert_eq!(calculate_key(1, 10, "src/test.txt").unwrap(), 50);
+        assert_eq!(calculate_key(2, 10, None, "src/test.txt").unwrap(), 50);
     }
 
     #[test]
     fn test_pt2_2() {
-        assert_eq!(calculate_key(1, 50, "src/test.txt").unwrap(), 1594);
+        assert_eq!(
+            calculate_key(2, 50, Some(vec![4, 5, 6]), "src/test.txt").unwrap(),
+            1594
+        );
     }
 
     #[test]
     fn test_pt2_3() {
-        assert_eq!(calculate_key(1, 100, "src/test.txt").unwrap(), 6536);
+        assert_eq!(
+            calculate_key(2, 100, Some(vec![4, 5, 6]), "src/test.txt").unwrap(),
+            6536
+        );
     }
 
     #[test]
     fn test_pt2_4() {
-        assert_eq!(calculate_key(1, 500, "src/test.txt").unwrap(), 167004);
+        assert_eq!(
+            calculate_key(2, 500, Some(vec![4, 5, 6]), "src/test.txt").unwrap(),
+            167004
+        );
     }
 
     #[test]
     fn test_pt2_5() {
-        assert_eq!(calculate_key(1, 1000, "src/test.txt").unwrap(), 668697);
+        assert_eq!(
+            calculate_key(2, 1000, Some(vec![4, 5, 6]), "src/test.txt").unwrap(),
+            668697
+        );
     }
 
     #[test]
     fn test_pt2_6() {
-        assert_eq!(calculate_key(1, 5000, "src/test.txt").unwrap(), 16733044);
+        assert_eq!(
+            calculate_key(2, 5000, Some(vec![4, 5, 6]), "src/test.txt").unwrap(),
+            16733044
+        );
     }
 }
