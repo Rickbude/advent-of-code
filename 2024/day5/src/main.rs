957cba3ff15c64f24ae166c16e852086c9ee690e
Rick Budé <24381938+Rickbude@users.noreply.github.com>
2024-12-21 01:39:35 +0100

    Improve solution day 5
    Found a neat trick in the solution megathread: just use the built
    in is_sorted_by and sort_by functions. Thanks SuperSmurfen! :)

--- 2024/day5/src/main.rs
+++ 2024/day5/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap,HashSet};
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{self, Read};
 
@@ -16,19 +16,17 @@ fn calculate_key(part: usize, filename: &str) -> io::Result<usize> {
     //Parse the rules (the first section of the input) Looking at the test data,
     //a single key can have multiple values. So we will use a hashmap with a
     //hashset to store the values.
-    let mut rules: HashMap<usize, HashSet<usize>> = HashMap::new();
-    for rule in rules_str.split_whitespace() {
-        let mut rule_parts = rule.split('|');
-        let page1 = rule_parts.next().unwrap().parse::<usize>().unwrap();
-        let page2 = rule_parts.next().unwrap().parse::<usize>().unwrap();
-        rules.entry(page1).or_insert_with(HashSet::new).insert(page2);
+    let mut rules = HashMap::<usize, HashSet<usize>>::new();
+    for rule in rules_str.lines() {
+        let (page1_str, page2_str) = rule.split_once('|').unwrap();
+        let page1: usize = page1_str.parse().unwrap();
+        let page2: usize = page2_str.parse().unwrap();
+        rules.entry(page1).or_default().insert(page2);
     }
 
     let mut key = 0;
 
     //Parse and immediately process the updates (second section of the input)
-    //Apparently this can be done efficiently with a fancy algorithm called a
-    //topological sort, but the runtime for this code is already only a few ms..
     for update in updates_str.split_whitespace() {
         //Split the update string into individual pages
         let mut pages: Vec<usize> = update
@@ -36,34 +34,18 @@ fn calculate_key(part: usize, filename: &str) -> io::Result<usize> {
             .map(|x| x.parse::<usize>().unwrap())
             .collect();
 
-        //In part 1, the loop is aborted after a single iteration. In part 2, we
-        //keep looping until all updates are corrected.
-        let mut fixed_rule: bool = false;
-        let mut in_order;
-        loop {
-            in_order = true;
-            for i in 0..pages.len() {
-                for j in i + 1..pages.len() {
-                    if let Some(dependencies) = rules.get(&pages[j]) {
-                        if dependencies.contains(&pages[i]) {
-                            in_order = false;
-                            //Update contains out-of-order elements, these need
-                            //to be swapped.
-                            if part == 2 {
-                                pages.swap(i, j);
-                                fixed_rule = true;
-                            }
-                        }
-                    }
-                }
+        //Use built-in is_sorted_by and sort_by functions, which is an
+        //improvement over the previous self-built bubble sort. Found this trick
+        //in the solution megathread (SuperSmurfen)
+        if pages.is_sorted_by(|a, b| !rules.contains_key(b) || !rules[b].contains(a)) {
+            if part == 1 {
+                key += &pages[pages.len() / 2];
             }
-            if part == 1 || in_order {
-                break;
+        } else {
+            if part == 2 {
+                pages.sort_by(|a, b| (!rules.contains_key(b) || !rules[b].contains(a)).cmp(&true));
+                key += &pages[pages.len() / 2];
             }
-        }
-        //Add the middle page number
-        if (part == 1 && in_order) || (part == 2 && fixed_rule) {
-            key += &pages[pages.len() / 2];
         }
     }
 
